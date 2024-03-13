923ad788a0b944e6a0ef20d07b3a6909da5ff964
Bruno Gomes <obrunogomes@obrunogomes.local>
2024-03-13 17:00:49 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from '@/components/header'
+import { View } from 'react-native'
+
+
+
+export default function Home(){
+    return(
+        <View className= "flex-1 p-4">
+            <Header/>
+        </View>
+    )
+}
+
