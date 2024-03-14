4926e341bc1177e584caa033dbd31684fb50b515
Bruno Gomes <obrunogomes@obrunogomes.local>
2024-03-14 18:34:18 -0300

    FlatList update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,33 @@
+import { CategoryButton } from '@/components/category-button'
 import { Header } from '@/components/header'
-import { View } from 'react-native'
+import { View, FlatList } from 'react-native'
+import { useState } from 'react'
+
+import { CATEGORIES} from "@/utils/data/products"
 
 
 
 export default function Home(){
+    const [category, setCategory] = useState(CATEGORIES[0])
+
+    function handleCategorySelect(selectedCategory: string){
+        setCategory(selectedCategory)
+    }
     return(
         <View className= "flex-1 p-4">
-            <Header/>
+            <Header title='Faça o seu pedido' cartQuantityIens={2}/>
+
+            <FlatList 
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({item}) => (
+                    <CategoryButton  title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)}/>
+                )}
+                horizontal
+                className='max-h-10 mt-5'
+                contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
+                showsHorizontalScrollIndicator={false}
+            />
         </View>
     )
 }
